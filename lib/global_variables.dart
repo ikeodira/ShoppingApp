2a032acca17ed12b5d5b0addb7efa2bb79e33fe6
Ike Odira <odiraike8@gmail.com>
2023-08-22 23:10:02 -0400

    Complete Detailed Page Design

---
+++ lib/global_variables.dart
@@ -0,0 +1,37 @@
+final products = [
+  {
+    "id": "0",
+    "title": "Men's Nike Shoes",
+    "price": 44.52,
+    "sizes": [8, 9, 10, 11],
+    "company": "Nike",
+    "imageUrl": "assets/image/shoe1.png",
+  },
+  {
+    "id": "1",
+    "title": "Adidas Shoes",
+    "price": 20.12,
+    "sizes": [8, 9, 10, 11],
+    "company": "Addidas",
+    "imageUrl": "assets/image/shoe2.png",
+  },
+  {
+    "id": "2",
+    "title": "Bata Men's Shoes",
+    "price": 28.95,
+    "sizes": [8, 9, 10, 11],
+    "company": "Bata",
+    "imageUrl": "assets/image/shoe3.png",
+  },
+];
+
+final cart = [
+  {
+    "id": "3",
+    "title": "Jordan Shoes",
+    "price": 420.69,
+    "imageUrl": "assets/images/shoes_4.png",
+    "company": "Nike",
+    "size": 8,
+  }
+];
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/global_variables.dart';
+import 'package:shop_app/home_page.dart';
+import 'package:shop_app/product_details_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping App',
+      theme: ThemeData(
+        fontFamily: "Lato",
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromRGBO(254, 206, 1, 1),
+          primary: const Color.fromRGBO(254, 206, 1, 1),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          hintStyle: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+          prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
+        ),
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 35,
+          ),
+          titleMedium: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+          bodySmall: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+        useMaterial3: true,
+      ),
+      home: ProductDetailsPage(
+        product: products[0],
+      ),
+    );
+  }
+}
